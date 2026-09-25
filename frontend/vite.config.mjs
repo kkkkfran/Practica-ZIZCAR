de66134601375a7a25729a42e1493ef65bbafef8
Francisco Jara <kaushxoficial@gmail.com>
2026-09-25 14:08:18 -0300

    ci: configure GitHub Pages automated deployment and Vite base URL

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -10,6 +10,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Practica-ZIZCAR/',
   plugins: [
     Vue({
       template: { transformAssetUrls },
